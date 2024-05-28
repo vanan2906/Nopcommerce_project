9af1a1534c14b4f2acdd1c89c229acd02c907260
VA <laptoppc>
2024-05-29 00:27:42 +0700

    webmanagerdriver

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -1,5 +1,6 @@
 package common;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -9,14 +10,31 @@ public class BaseTest {
     public WebDriver driver;
 
     public WebDriver getDriverBrowser(String browserName) {
-        if (browserName.equals("firefox")) {
-            driver = new FirefoxDriver();
-        } else if (browserName.equals("chrome")) {
-            driver = new ChromeDriver();
-        } else if (browserName.equals("edge")) {
-            driver = new EdgeDriver();
-        } else {
-            throw new RuntimeException("Browser name invalid");
+//        if (browserName.equals("firefox")) {
+//            driver = new FirefoxDriver();
+//        } else if (browserName.equals("chrome")) {
+//            driver = new ChromeDriver();
+//        } else if (browserName.equals("edge")) {
+//            driver = new EdgeDriver();
+//        } else {
+//            throw new RuntimeException("Browser name invalid");
+//        }
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                      break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser name invalid");
         }
         driver.get("https://demo.nopcommerce.com/");
         return driver;
